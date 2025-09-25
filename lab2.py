9dffa0a8cbb2825c9e43537361a3de4ffb05f273
nellezxc123163 <98385625+nellezxc123163@users.noreply.github.com>
2025-09-25 19:22:13 +0400

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -2,9 +2,7 @@ import math
 from math import *
 x = 0.1 # начало отрезка а
 z = 1.2 # начало отрезка b
-while x <= z:
-    x = 0.1
-    z = 1.2
+while x < z:
     x += 0.05
     y = sin(4**x-log(3**x,math.e))
     print(y)
